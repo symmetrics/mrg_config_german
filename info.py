0e0322588d463284194ade727acdce1b5244001f
Sergej Braznikov <sb@ryotic.de>
2009-04-14 12:01:35 +0200

    Neue Versionnummer vergeben; info.py nach neue Format gestaltet;

--- info.py
+++ info.py
@@ -1,43 +1,63 @@
 # encoding: utf-8
 
+# =============================================================================
 # package info
-name = 'symmetrics_config_german'
-tags = ('magento', 'config', 'german', 'symmetrics')
+# =============================================================================
+NAME = 'symmetrics_config_german'
 
+TAGS = ('magento', 'module', 'symmetrics', 'config', 'german')
+
+LICENSE = 'AFL 3.0'
+
+HOMEPAGE = 'http://www.symmetrics.de'
+
+INSTALL_PATH = ''
+
+# =============================================================================
+# responsibilities
+# =============================================================================
+TEAM_LEADER = {
+    'Sergej Braznikov': 'sb@symmetrics.de'
+}
+
+MAINTAINER = {
+    'Eugen Gitin': 'eg@symmetrics.de'
+}
+
+AUTHORS = {
+    'Eugen Gitin': 'eg@symmetrics.de'
+}
+
+# =============================================================================
+# additional infos
+# =============================================================================
+INFO = 'symmetrics Basiskonfiguration für deutsche Shops'
+
+SUMMARY = '''
+    symmetrics Basiskonfiguration für deutsche Shops
+'''
+
+NOTES = '''
+'''
+
+# =============================================================================
 # relations
-requires = {
+# =============================================================================
+REQUIRES = {
     'magento': '*',
     'symmetrics_module_impressum': '*',
     'symmetrics_module_invoicepdf': '*',
     'mc_module_locale_mage_community_de_de': '*',
 }
-excludes = {
+
+EXCLUDES = {
 }
 
-# who is responsible for this package?
-team_leader = {
-    'Sergej Braznikov': 'sb@symmetrics.de'
-}
-
-# who should check this package in the first place?
-maintainer = {
-    'Eugen Gitin': 'eg@symmetrics.de'
-}
-
-# relative installation path (e.g. app/code/local)
-install_path = ''
-
-# additional infos
-info = 'symmetrics Basiskonfiguration für deutsche Shops'
-summary = '''
-    symmetrics Basiskonfiguration für deutsche Shops
-'''
-license = 'AFL 3.0'
-authors = {
-    'Eugen Gitin': 'eg@symmetrics.de'
-}
-homepage = 'http://www.symmetrics.de'
-
-# files this package depends on
-depends_on_files = (
+DEPENDS_ON_FILES = (
 )
+
+PEAR_KEY = ''
+
+COMPATIBLE_WITH = {
+    'magento': '1.3.0'
+}
