3c84ab8cc6825f4858ffe4acbfed59c3151ec263
Eugen Gitin <devnull@localhost>
2009-05-07 20:44:10 +0200

    HTMLs und Texte in ein neues Modul extrahieren; Konfiguration ausschliesslich über die XML-Datei config.xml

--- build/app/code/local/Symmetrics/ConfigGerman/sql/symconfig_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$installer = $this;
-$installer->startSetup();
-
-$installer->setConfigData('tax/calculation/apply_after_discount', '0');
-$installer->setConfigData('tax/calculation/discount_tax', '1');
-
-$installer->endSetup();
