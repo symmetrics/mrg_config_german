af0a7d28bfccdc687738db7066a9da9dfa4e6ac0
Eugen Gitin <eg@symmetrics.de>
2010-01-26 20:15:40 +0100

    Compatibility in info.py angepasst

--- info.py
+++ info.py
@@ -58,6 +58,6 @@ DEPENDS_ON_FILES = (
 PEAR_KEY = ''
 
 COMPATIBLE_WITH = {
-    'magento': ['1.3.2.1', '1.3.2.3', '1.3.2.4'],
-	'magento_enterprise': ['1.3.2.4']
+    'magento': ['1.4.0.0'],
+	'magento_enterprise': ['1.7.0.0']
 }
