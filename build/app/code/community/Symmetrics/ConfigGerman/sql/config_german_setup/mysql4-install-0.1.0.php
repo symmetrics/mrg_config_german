36dab477c2db2842835c427064856955f353c925
Siegfried Schmitz <ss@symmetrics.de>
2010-04-13 11:17:48 +0200

    Datenbankfelder für die addAttributes-Methode auf Magento 1.4.0.0 angepasst (heißen jetzt anders als vorher)

--- build/app/code/community/Symmetrics/ConfigGerman/sql/config_german_setup/mysql4-install-0.1.0.php
+++ build/app/code/community/Symmetrics/ConfigGerman/sql/config_german_setup/mysql4-install-0.1.0.php
@@ -311,15 +311,15 @@ $installer->addAttribute(
     array(
         'label' => 'Gewicht',
         'input' => 'text',
-        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-        'visible' => true,
-        'required' => true,
-        'user_defined' => true,
-        'searchable' => true,
-        'comparable' => true,
-        'visible_on_front' => true,
-        'visible_in_advanced_search' => true,
-        'default' => '1'
+        'is_global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+        'is_visible' => true,
+        'is_required' => true,
+        'is_user_defined' => true,
+        'is_searchable' => true,
+        'is_comparable' => true,
+        'is_visible_on_front' => true,
+        'is_visible_in_advanced_search' => true,
+        'default_value' => '1'
     )
 );
 
